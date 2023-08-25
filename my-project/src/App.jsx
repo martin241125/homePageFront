47523ffccc74faefe1e1b37494aa703708d3c0df
martin241125 <martincitooo21@gmail.com>
2023-08-25 20:54:34 -0300

    aprendiendo

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,12 +1,35 @@
+import logo from '../../assets/images/logo.svg'
+import img3 from '../../assets/images/image-web-3-desktop.jpg'
 
-import './App.css'
 
 function App() {
   
 
   return (
     <>
-     <h1 className='bg-red-500 text-xl'>hola mundo</h1>
+     <nav className='flex flex-auto'>
+      
+      <img src={logo} alt="logo" className='mr-auto pt-3 pl-3 '/>
+      <ul className="flex flex-row justify-end pt-3 pr-3">
+        <li className="m-2 hover:cursor-pointer hover:text-orange-400">Home</li>
+        <li className="m-2 hover:cursor-pointer hover:text-orange-400">New</li>
+        <li className="m-2 hover:cursor-pointer hover:text-orange-400">Popular</li>
+        <li className="m-2 hover:cursor-pointer hover:text-orange-400">Trendig</li>
+        <li className="m-2 hover:cursor-pointer hover:text-orange-400">Categories</li>
+      </ul>
+     </nav>
+     <main className='flex flex-row bg-black '>
+     <section>
+      <div className=' '>
+        <img src={img3} alt="formas geometricas" className='ml-2 mt-10'/>
+      </div>
+     </section>
+     <section className=''>
+        <div className='bg-slate-700 '>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, quia facilis? Autem consequuntur iusto praesentium quas rerum dignissimos temporibus ipsa optio aperiam, iure sed exercitationem similique asperiores, tempora, itaque nihil.</p>
+        </div>
+     </section>
+     </main>
         
     </>
   )
