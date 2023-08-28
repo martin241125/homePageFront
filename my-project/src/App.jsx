bad31eb1031f6f041b7c367620b04150d4b751b1
martin241125 <martincitooo21@gmail.com>
2023-08-27 21:45:08 -0300

    seguimos

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,38 +1,109 @@
-import logo from '../../assets/images/logo.svg'
-import img3 from '../../assets/images/image-web-3-desktop.jpg'
-
+import logo from "../../assets/images/logo.svg";
+import img3 from "../../assets/images/image-web-3-desktop.jpg";
+import img01 from "../../assets/images/image-retro-pcs.jpg";
+import img02 from "../../assets/images/image-top-laptops.jpg";
+import img03 from "../../assets/images/image-gaming-growth.jpg";
 
 function App() {
-  
-
   return (
     <>
-     <nav className='flex flex-auto'>
-      
-      <img src={logo} alt="logo" className='mr-auto pt-3 pl-3 '/>
-      <ul className="flex flex-row justify-end pt-3 pr-3">
-        <li className="m-2 hover:cursor-pointer hover:text-orange-400">Home</li>
-        <li className="m-2 hover:cursor-pointer hover:text-orange-400">New</li>
-        <li className="m-2 hover:cursor-pointer hover:text-orange-400">Popular</li>
-        <li className="m-2 hover:cursor-pointer hover:text-orange-400">Trendig</li>
-        <li className="m-2 hover:cursor-pointer hover:text-orange-400">Categories</li>
-      </ul>
-     </nav>
-     <main className='flex flex-row bg-black '>
-     <section>
-      <div className=' '>
-        <img src={img3} alt="formas geometricas" className='ml-2 mt-10'/>
-      </div>
-     </section>
-     <section className=''>
-        <div className='bg-slate-700 '>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, quia facilis? Autem consequuntur iusto praesentium quas rerum dignissimos temporibus ipsa optio aperiam, iure sed exercitationem similique asperiores, tempora, itaque nihil.</p>
+      <header>
+        <nav className="flex flex-row">
+          <img src={logo} alt="logo" className="mr-auto pt-3 pl-3 " />
+          <ul className="flex flex-row justify-end pt-3 pr-3">
+            <li className="m-2 hover:cursor-pointer hover:text-orange-400">
+              Home
+            </li>
+            <li className="m-2 hover:cursor-pointer hover:text-orange-400">
+              New
+            </li>
+            <li className="m-2 hover:cursor-pointer hover:text-orange-400">
+              Popular
+            </li>
+            <li className="m-2 hover:cursor-pointer hover:text-orange-400">
+              Trendig
+            </li>
+            <li className="m-2 hover:cursor-pointer hover:text-orange-400">
+              Categories
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <section className="border-2 border-rose-500 flex flex-row">
+        <div className="flex flex-col items-center">
+          <img src={img3} alt="formas geometricas" className="p-1  max-w-3xl" />
+          <article className="flex flex-row items-center">
+            <h1 className="font-haken text-3xl p-2">The Bright Future of Web 3.0?</h1>
+
+            <div>
+              <p className="font-inter p-2 text-justify">
+                We dive into the next evolution of the web that claims to put
+                the power of the platforms back into the hands of the people.
+                But is it really fulfilling its promise?
+              </p>
+              
+            <button className="bg-red-400 py-2 px-6 mb-1 text-white  font-inter">Read more</button>
+            </div>
+          </article>
         </div>
-     </section>
-     </main>
-        
+        <div className="">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam,
+            quia facilis? Autem consequuntur iusto praesentium quas rerum
+            dignissimos temporibus ipsa optio aperiam, iure sed exercitationem
+            similique asperiores, tempora, itaque nihil.
+          </p>
+        </div>
+        <div>
+          <h2>New</h2>
+          <article>
+            <h3>Hydrogen VS Electric Cars </h3>
+            <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+          </article>
+          <article>
+            <h3>The Downsides of AI Artistry</h3>
+            <p>
+              What are the possible adverse effects of on-demand AI image
+              generation?
+            </p>
+          </article>
+          <article>
+            <h3>Is VC Funding Drying Up? </h3>
+            <p>
+              Private funding by VC firms is down 50% YOY. We take a look at
+              what that means.
+            </p>
+          </article>
+        </div>
+      </section>
+      <div>
+        <article>
+          <img src={img01} alt="" />
+          <div>
+            <span> 01</span>
+            <h3>Reviving Retro PCs</h3>
+            <p> What happens when old PCs are given modern upgrades?</p>
+          </div>
+        </article>
+        <article>
+          <img src={img02} alt="" />
+          <div>
+            <span> 02</span>
+            <h3>Top 10 Laptops of 2022</h3>
+            <p>Our best picks for various needs and budgets.</p>
+          </div>
+        </article>
+        <article>
+          <img src={img03} alt="" />
+          <div>
+            <span>03 </span>
+            <h3>The Growth of Gaming</h3>
+            <p> How the pandemic has sparked fresh opportunities.</p>
+          </div>
+        </article>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
